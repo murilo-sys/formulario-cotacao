39dce428d9d8d2cbbb3d90317b458361906d511b
Murilo <murilod_santiago@hotmail.com>
2026-07-28 17:38:53 -0300

    fix: não substrai a ultima linha, apaga ela por completo

--- src/components/LinhasCubagens.tsx
+++ src/components/LinhasCubagens.tsx
@@ -68,17 +68,17 @@ export default function LinhasCubagens({ totalVolumes: totalVolumesDigitado }: L
                 return
             }
 
-            // //Caso a ultima linha se fosse tirada, ela seria menor que o valor total
-            // if ((Number(totalVolumesSomados) - Number(cubagens[fields.length - 1].quantidade)) < Number(totalVolumesDigitado)) {
+            //Caso a ultima linha se fosse tirada, ela seria menor que o valor total
+            if ((Number(totalVolumesSomados) - Number(cubagens[fields.length - 1].quantidade)) < Number(totalVolumesDigitado)) {
 
-            //     //Faz essa formula que nem eu entendi como eu fiz, mas retorna o valor 
-            //     const valorUltimaLinha = Math.abs((Number(totalVolumesSomados) - Number(cubagens[fields.length - 1].quantidade)) - Number(totalVolumesDigitado))
+                //Faz essa formula que nem eu entendi como eu fiz, mas retorna o valor 
+                // const valorUltimaLinha = Math.abs((Number(totalVolumesSomados) - Number(cubagens[fields.length - 1].quantidade)) - Number(totalVolumesDigitado))
 
-            //     //Seta o valor na cubagem
-            //     setValue(`cubagens.${fields.length - 1}.quantidade`, String(valorUltimaLinha))
+                //Seta o valor na cubagem
+                setValue(`cubagens.${fields.length - 1}.quantidade`, "")
 
-            //     return
-            // }
+                return
+            }
 
             remove(fields.length - 1)
 
@@ -88,126 +88,124 @@ export default function LinhasCubagens({ totalVolumes: totalVolumesDigitado }: L
 
     return (
         <div className="flex flex-col gap-1">
-            <AnimatePresence>
-                {fields.map((field, index) => (
-                    <motion.div
-                        initial={{ opacity: 0, height: 0 }}
-                        animate={{ opacity: 1, height: "auto" }}
-                        transition={{ duration: 0.1, ease: "easeOut" }}
-                        exit={{ opacity: 0, height: 0 }}
-                        key={field.id}>
+            {fields.map((field, index) => (
+                <motion.div
+                    initial={{ opacity: 0, height: 0 }}
+                    animate={{ opacity: 1, height: "auto" }}
+                    transition={{ duration: 0.1, ease: "easeOut" }}
+                    exit={{ opacity: 0, height: 0 }}
+                    key={field.id}>
 
-                        <div className="flex flex-col border-b pb-3 border-blue-500 border-dotted lg:border-none lg:grid lg:grid-cols-4 lg:grid-rows-1 gap-3">
+                    <div className="flex flex-col border-b pb-3 border-blue-500 border-dotted lg:border-none lg:grid lg:grid-cols-4 lg:grid-rows-1 gap-3">
 
-                            <div className="flex flex-col">
-                                <Label htmlFor={`quantidade${index}`} className="font-light" obrigatorio={true}>Volumes</Label>
-                                <Controller
-                                    name={`cubagens.${index}.quantidade`}
-                                    control={control}
-                                    render={({ field }) => {
-                                        return <Input
-                                            className="pl-12"
-                                            ref={field.ref}
-                                            prefixo="UN"
-                                            erro={errors.cubagens?.[index]?.quantidade?.message}
-                                            id={`quantidade${index}`}
-                                            value={field.value || ""}
-                                            onChange={(e) => {
-                                                clearErrors(`cubagens.${index}.quantidade`)
+                        <div className="flex flex-col">
+                            <Label htmlFor={`quantidade${index}`} className="font-light" obrigatorio={true}>Volumes</Label>
+                            <Controller
+                                name={`cubagens.${index}.quantidade`}
+                                control={control}
+                                render={({ field }) => {
+                                    return <Input
+                                        className="pl-12"
+                                        ref={field.ref}
+                                        prefixo="UN"
+                                        erro={errors.cubagens?.[index]?.quantidade?.message}
+                                        id={`quantidade${index}`}
+                                        value={field.value || ""}
+                                        onChange={(e) => {
+                                            clearErrors(`cubagens.${index}.quantidade`)
 
-                                                // soma os volumes das outras linhas exceto a atual.
-                                                const volumesOutrasLinhas = cubagens.reduce((acumulador, item, i) => {
-                                                    if (i === index) return acumulador; // Ignora a própria linha!
-                                                    return acumulador + (Number(item?.quantidade) || 0);
-                                                }, 0);
+                                            // soma os volumes das outras linhas exceto a atual.
+                                            const volumesOutrasLinhas = cubagens.reduce((acumulador, item, i) => {
+                                                if (i === index) return acumulador; // Ignora a própria linha!
+                                                return acumulador + (Number(item?.quantidade) || 0);
+                                            }, 0);
 
-                                                // O máximo que esta linha pode ter é o total MENOS as outras linhas
-                                                const maxPermitido = Number(totalVolumesDigitado) - volumesOutrasLinhas
-                                                const valorDigitado = Number(e.target.value) || 0
+                                            // O máximo que esta linha pode ter é o total MENOS as outras linhas
+                                            const maxPermitido = Number(totalVolumesDigitado) - volumesOutrasLinhas
+                                            const valorDigitado = Number(e.target.value) || 0
 
-                                                if (valorDigitado > maxPermitido) {
-                                                    field.onChange(String(maxPermitido))
-                                                    return
-                                                }
+                                            if (valorDigitado > maxPermitido) {
+                                                field.onChange(String(maxPermitido))
+                                                return
+                                            }
 
-                                                field.onChange(e.target.value);
-                                            }}
-                                        />
-                                    }}
-                                />
-                            </div>
-
-                            <div className="flex flex-col">
-                                <Label htmlFor={`comprimento${index}`} className="font-light" obrigatorio={true}>Comprimento</Label>
-                                <Controller
-                                    name={`cubagens.${index}.comprimento`}
-                                    control={control}
-                                    render={({ field }) => {
-                                        return <Input
-                                            className="pl-10"
-                                            ref={field.ref}
-                                            prefixo="M"
-                                            erro={errors.cubagens?.[index]?.comprimento?.message}
-                                            id={`comprimento${index}`}
-                                            value={field.value || ""}
-                                            onChange={(e) => {
-                                                clearErrors(`cubagens.${index}.comprimento`)
-                                                field.onChange(e.target.value)
-                                            }}
-                                        />
-                                    }}
-                                />
-                            </div>
-
-                            <div className="flex flex-col">
-                                <Label htmlFor={`largura${index}`} className="font-light" obrigatorio={true}>Largura</Label>
-                                <Controller
-                                    name={`cubagens.${index}.largura`}
-                                    control={control}
-                                    render={({ field }) => {
-                                        return <Input
-                                            className="pl-10"
-                                            ref={field.ref}
-                                            prefixo="M"
-                                            erro={errors.cubagens?.[index]?.largura?.message}
-                                            id={`largura${index}`}
-                                            value={field.value || ""}
-                                            onChange={(e) => {
-                                                clearErrors(`cubagens.${index}.largura`)
-                                                field.onChange(e.target.value)
-                                            }}
-                                        />
-                                    }}
-                                />
-                            </div>
-
-                            <div className="flex flex-col">
-                                <Label htmlFor={`altura${index}`} className="font-light" obrigatorio={true}>Altura</Label>
-                                <Controller
-                                    name={`cubagens.${index}.altura`}
-                                    control={control}
-                                    render={({ field }) => {
-                                        return <Input
-                                            className="pl-10"
-                                            ref={field.ref}
-                                            prefixo="M"
-                                            erro={errors.cubagens?.[index]?.altura?.message}
-                                            id={`altura${index}`}
-                                            value={field.value || ""}
-                                            onChange={(e) => {
-                                                clearErrors(`cubagens.${index}.altura`)
-                                                field.onChange(e.target.value)
-                                            }}
-                                        />
-                                    }}
-                                />
-                            </div>
-
+                                            field.onChange(e.target.value);
+                                        }}
+                                    />
+                                }}
+                            />
                         </div>
 
-                    </motion.div>
-                ))}
-            </AnimatePresence>
+                        <div className="flex flex-col">
+                            <Label htmlFor={`comprimento${index}`} className="font-light" obrigatorio={true}>Comprimento</Label>
+                            <Controller
+                                name={`cubagens.${index}.comprimento`}
+                                control={control}
+                                render={({ field }) => {
+                                    return <Input
+                                        className="pl-10"
+                                        ref={field.ref}
+                                        prefixo="M"
+                                        erro={errors.cubagens?.[index]?.comprimento?.message}
+                                        id={`comprimento${index}`}
+                                        value={field.value || ""}
+                                        onChange={(e) => {
+                                            clearErrors(`cubagens.${index}.comprimento`)
+                                            field.onChange(e.target.value)
+                                        }}
+                                    />
+                                }}
+                            />
+                        </div>
+
+                        <div className="flex flex-col">
+                            <Label htmlFor={`largura${index}`} className="font-light" obrigatorio={true}>Largura</Label>
+                            <Controller
+                                name={`cubagens.${index}.largura`}
+                                control={control}
+                                render={({ field }) => {
+                                    return <Input
+                                        className="pl-10"
+                                        ref={field.ref}
+                                        prefixo="M"
+                                        erro={errors.cubagens?.[index]?.largura?.message}
+                                        id={`largura${index}`}
+                                        value={field.value || ""}
+                                        onChange={(e) => {
+                                            clearErrors(`cubagens.${index}.largura`)
+                                            field.onChange(e.target.value)
+                                        }}
+                                    />
+                                }}
+                            />
+                        </div>
+
+                        <div className="flex flex-col">
+                            <Label htmlFor={`altura${index}`} className="font-light" obrigatorio={true}>Altura</Label>
+                            <Controller
+                                name={`cubagens.${index}.altura`}
+                                control={control}
+                                render={({ field }) => {
+                                    return <Input
+                                        className="pl-10"
+                                        ref={field.ref}
+                                        prefixo="M"
+                                        erro={errors.cubagens?.[index]?.altura?.message}
+                                        id={`altura${index}`}
+                                        value={field.value || ""}
+                                        onChange={(e) => {
+                                            clearErrors(`cubagens.${index}.altura`)
+                                            field.onChange(e.target.value)
+                                        }}
+                                    />
+                                }}
+                            />
+                        </div>
+
+                    </div>
+
+                </motion.div>
+            ))}
         </div>
     )
 }
