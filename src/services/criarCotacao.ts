864e9b4986a37c8832d683295286ac745d7cf353
Murilo <murilod_santiago@hotmail.com>
2026-08-12 08:47:25 -0300

    feat: adicionado valores mocados para deploy 2.0

--- src/services/criarCotacao.ts
+++ src/services/criarCotacao.ts
@@ -1,5 +1,11 @@
 import { CotacaoCompletaDados, CriarCotacaoResponse } from "@/schemas/cotacaoSchema";
 
 export default async function criarCotacao(formularioDados: CotacaoCompletaDados): Promise<CriarCotacaoResponse> {
-  console.log(formulario);
+  console.log(formularioDados);
+
+  return {
+    valor: "100",
+    codigo: "10",
+    dataValidade: "10"
+  };
 }
