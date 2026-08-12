6f122e46f523153ef3dbd5c5cb28400e2e9f0afb
Murilo <murilod_santiago@hotmail.com>
2026-08-12 15:02:50 -0300

    refactor: arquivo renomeado

---
+++ src/services/server/adapters/simularValor.ts
@@ -0,0 +1,42 @@
+import 'server-only'
+
+import { CotacaoDados } from "@/schemas/cotacaoSchema"
+import axios from "axios"
+
+interface CotacaoDadosCompleto extends CotacaoDados {
+    pesoTaxado: number,
+}
+
+export async function apiSimularValor(modal: "rodo" | "air", dados: CotacaoDadosCompleto, token: string, difalOpcao: boolean) {
+
+    //URL do endpoint
+    const url = "https://globalcargo.eslcloud.com.br/api/quote/calculate_freights"
+
+    //Retorna a requisição do axios (basicamente um fetch)
+    return await axios.get(url, {
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+            "Accept": "*/*"
+        },
+
+        //Data substituiu o body do fetch
+        data:
+        {
+            "data": {
+                "attributes": {
+                    "origin_postal_code": dados.cepOrigem,
+                    "destination_postal_code": dados.cepDestino,
+
+                    // CNPJ USADO PARA CALCULO DE DIFAL
+                    "recipient_document": `${difalOpcao ? "65971717000126" : ""}`,
+
+                    "real_weight": dados.pesoTaxado,
+                    "invoices_value": dados.valorNfe,
+                    "invoices_volumes": dados.totalVolumes,
+                    "modal": modal
+                }
+            }
+        }
+    })
+}
