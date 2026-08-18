608bb55741ad48f4c2a1d8c63889799d0d6a8f87
Murilo <murilod_santiago@hotmail.com>
2026-08-18 11:06:52 -0300

    feat: adicionado campos de endereço para remetente e destinatario

--- src/hooks/useEndereco.ts
+++ src/hooks/useEndereco.ts
@@ -1,56 +1,54 @@
-import { CotacaoDados } from "@/schemas/cotacaoSchema"
-import validarCep from "@/utils/validarCep"
-import { useRef, useState } from "react"
-import { useFormContext } from "react-hook-form"
+import { CotacaoDados } from "@/schemas/cotacaoSchema";
+import validarCep from "@/utils/validarCep";
+import { useRef, useState } from "react";
+import { useFormContext } from "react-hook-form";
 
 export function useEndereco() {
+  // Nome Rua Origem - Nome Rua Destino
+  const [enderecoOrigem, setEnderecoOrigem] = useState<string>("");
+  const [enderecoDestino, setEnderecoDestino] = useState<string>("");
 
-    // Nome Rua Origem - Nome Rua Destino
-    const [enderecoOrigem, setEnderecoOrigem] = useState<string>("")
-    const [enderecoDestino, setEnderecoDestino] = useState<string>("")
+  // Cep origem - Cep destino /-/-/
+  // Usado para quando o usuario clicar no campo, guardar o valor,
+  // e se quando ele sair, verificar se ambos são iguais, se for igual, não verifica novamente na API
+  const cepOrigemRef = useRef<string>("");
+  const cepDestinoRef = useRef<string>("");
 
-    // Cep origem - Cep destino /-/-/ 
-    // Usado para quando o usuario clicar no campo, guardar o valor, 
-    // e se quando ele sair, verificar se ambos são iguais, se for igual, não verifica novamente na API
-    const cepOrigemRef = useRef<string>("")
-    const cepDestinoRef = useRef<string>("")
+  // Context do useForm principal
+  const { setError } = useFormContext<CotacaoDados>();
 
-    // Context do useForm principal
-    const { setError } = useFormContext<CotacaoDados>()
+  //Função de consultarCep no momento do onBlur
+  async function consultarCepBlur(valorAtual: string, campo: "cepOrigem" | "cepDestino") {
+    const refAtual = campo === "cepOrigem" ? cepOrigemRef : cepDestinoRef;
+    const setEndereco = campo === "cepOrigem" ? setEnderecoOrigem : setEnderecoDestino;
 
-    //Função de consultarCep no momento do onBlur
-    async function consultarCepBlur(valorAtual: string, campo: "cepOrigem" | "cepDestino") {
-        const refAtual = campo === "cepOrigem" ? cepOrigemRef : cepDestinoRef
-        const setEndereco = campo === "cepOrigem" ? setEnderecoOrigem : setEnderecoDestino
+    // Se não foi alterado o texto, retorna
+    if (refAtual.current === valorAtual) return;
 
-        // Se não foi alterado o texto, retorna
-        if (refAtual.current === valorAtual) return
-
-        // Caso o campo esteja vazio
-        if (valorAtual.trim() === "") {
-            setEndereco("")
-            return
-        }
-
-        //Valida e consulta o cep Atual
-        const { cepValido, cidade, estado } = await validarCep(valorAtual)
-
-        //Caso o cep não seja válido
-        if (cepValido === false) {
-            setError(campo, { type: "manual", message: "Cep Inválido" })
-            return
-        }
-
-        if (cidade && estado) setEndereco(`${cidade} - ${estado}`)
+    // Caso o campo esteja vazio
+    if (valorAtual.trim() === "") {
+      setEndereco("");
+      return;
     }
 
-    // Exporta o que a tela precisa usar:
-    return {
-        enderecoOrigem,
-        enderecoDestino,
-        cepOrigemRef,
-        cepDestinoRef,
-        consultarCepBlur
+    //Valida e consulta o cep Atual
+    const { cepValido, cidade, estado } = await validarCep(valorAtual);
+
+    //Caso o cep não seja válido
+    if (cepValido === false) {
+      setError(campo, { type: "manual", message: "Cep Inválido" });
+      return;
     }
 
+    if (cidade && estado) setEndereco(`${cidade} - ${estado}`);
+  }
+
+  // Exporta o que a tela precisa usar:
+  return {
+    enderecoOrigem,
+    enderecoDestino,
+    cepOrigemRef,
+    cepDestinoRef,
+    consultarCepBlur
+  };
 }
